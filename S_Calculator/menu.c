c622164ffa7131b8f69b7e0d7fd8d5aede2e4c00
VlBrazhnik <vladislav-brazhnikov@mail.ru>
2021-07-09 10:43:47 +0700

    HM_Static_Calculator

---
+++ S_Calculator/menu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void menu() {
+
+	printf("Enter a num for:\n");
+	printf("1) Addition\n");
+	printf("2) Subtraction\n");
+	printf("3) Multiply\n");
+	printf("4) Division\n");
+	printf("5) Exit\n");
+}
